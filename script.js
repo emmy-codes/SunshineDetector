1458c171aa5a9f5d92dd597eca14be912a1a8635
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-11-23 09:17:52 +0100

    Switched API

--- script.js
+++ script.js
@@ -17,35 +17,14 @@ const getLocation = navigator.geolocation.getCurrentPosition(success, error, opt
       }
 
       function getAPI (x, y) {
-      const lat = x
-      const long = y
-      const url = "https://cors-anywhere.herokuapp.com/https://api.sunrise-sunset.org/json"
-      const requestURL = url.concat("?lat=", lat, "&lng=", long)
-
-      console.log(requestURL)
-      fetch(requestURL)
-      .then(response => response.json())
-      .then(data => console.log(data));
+        const lat = x
+        const long = y
+        const url = "https://cors-anywhere.herokuapp.com/https://api.geodatasource.com/city"
+        const apiKey = "S5RRHG5VS6G7CPBGRXUYUAVFS44W6ZXO"
+  
+        const requestURL = url.concat("?key=",apiKey,"&lat=", lat, "&lng=", long)
+  
+        fetch(requestURL)
+        .then(response => response.json())
+        .then(data => console.log(data));
       }
-
-
-
-
-
-
-      // const lat = x
-      // const long = y
-      // const url = "https://api.geodatasource.com/city"
-      // const apiKey = "S5RRHG5VS6G7CPBGRXUYUAVFS44W6ZXO"
-
-      // const requestURL = url.concat("?key=",apiKey,"&lat=", lat, "&lng=", long)
-
-      // fetch(requestURL)
-      // .then(response => response.json())
-      // .then(data => console.log(data));
-
-
-      // // let apiAddress = `https://api.geodatasource.com/city?key=S5RRHG5VS6G7CPBGRXUYUAVFS44W6ZXO&lat=${lat}&lng=${long}`;
-      // //   fetch(apiAddress)
-      // //   .then(response => response.json())
-      // //   .then(data => console.log(data));
