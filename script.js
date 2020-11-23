13572568d3e8daf08010f2f88ab0a8d09aa5b51c
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-11-23 15:21:19 +0100

    Solution for the mountains
    This is maybe not the best solution for the mountains, but could not think of anything else. There is a lot of CSS code and i have med one Media query for really small phones so the mountains doesnt cover the text...

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ const options = {
     maximumAge: 30000, 
     timeout: 10000
   };
-const getLocation = navigator.geolocation.getCurrentPosition(success, error, options);
+// const getLocation = navigator.geolocation.getCurrentPosition(success, error, options);
 
-    function success(position) {
-        let latitude = position.coords.latitude;
-        let longitude = position.coords.longitude;
-        console.log(longitude, latitude)
-        getAPI(latitude, longitude);
-    }
+    // function success(position) {
+    //     let latitude = position.coords.latitude;
+    //     let longitude = position.coords.longitude;
+    //     console.log(longitude, latitude)
+    //     getAPI(latitude, longitude);
+    // }
 
-    function error () {
-        alert('Sorry, no position available.');
-      }
+    // function error () {
+    //     alert('Sorry, no position available.');
+    //   }
 
-      function getAPI (x, y) {
-        const lat = x
-        const long = y
-        const url = "https://cors-anywhere.herokuapp.com/https://api.geodatasource.com/city"
-        const apiKey = "S5RRHG5VS6G7CPBGRXUYUAVFS44W6ZXO"
+    //   function getAPI (x, y) {
+    //     const lat = x
+    //     const long = y
+    //     const url = "https://cors-anywhere.herokuapp.com/https://api.geodatasource.com/city"
+    //     const apiKey = "S5RRHG5VS6G7CPBGRXUYUAVFS44W6ZXO"
   
-        const requestURL = url.concat("?key=",apiKey,"&lat=", lat, "&lng=", long)
+    //     const requestURL = url.concat("?key=",apiKey,"&lat=", lat, "&lng=", long)
   
-        fetch(requestURL)
-        .then(response => response.json())
-        .then(data => console.log(data));
-      }
+    //     fetch(requestURL)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+    //   }
