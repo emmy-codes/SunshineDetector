c55ecc7ea67e49454ad8929e81b0b92154e9a81f
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-11-24 12:39:28 +0100

    Alternative error page
    I did an alternative error page that looks more like what we decided instead of the modal that i created. I think it looks better too and more as we wanted :smile:

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const date = document.getElementById("date");
+const sunsetPrint = document.getElementById("time");
+const heading = document.getElementById("info");
+const counter = document.getElementById("countdown");
+const sun = document.getElementById("sun");
+const centerText = document.getElementById("center-text");
 const variations = {weekday : "long", month : "long", day : "numeric", year : "numeric"}
 const today = new Date();
-const errorModal = document.getElementById("error-modal")
+const errorModal = document.getElementById("error-modal");
+
 
 date.innerHTML = today.toLocaleDateString("en-US", variations);
 
@@ -48,7 +54,7 @@ const getLocation = navigator.geolocation.getCurrentPosition(success, error, opt
           let sunset = data.sunset;
           // console.log(sunset);
   
-          let sunsetPrint = document.getElementById("time");
+          
           sunsetPrint.innerText = "Sunset: " + sunset;
           
       })
@@ -58,12 +64,33 @@ const getLocation = navigator.geolocation.getCurrentPosition(success, error, opt
       };
 
       function displayErrorPage() {
-        errorModal.style.display = "block";
-        const refreshButton = document.getElementById("refresh-icon");
+        errorModal.style.display = "none";
+        document.body.style.background = "var(--dark-gray)";
 
+        sun.style.display = "none";
+        sunsetPrint.style.display = "none";
+
+        date.style.color = "var(--light-peach)";
+        centerText.style.marginTop = "15vh"
+        
+        heading.style.color = "var(--light-peach)";
+        heading.style.fontSize = "3rem";
+        heading.style.fontFamily = "var(--font-numbers)";
+        heading.innerText = "Tripped when looking for sunshine :(";
+
+        counter.style.fontFamily = "var(--font-text)";
+        counter.style.fontWeight = "700";
+        counter.style.fontSize = "2rem";
+        counter.innerHTML = "We could not find your location! Please refresh the page and allow us to see your location." + 
+        "<br/>" + "<div id=\"refresh-icon\"><i class=\"fas fa-redo\"></i></div>";
+
+        counter.style.color = "var(--light-peach)";
+
+        const refreshButton = document.getElementById("refresh-icon");
         refreshButton.addEventListener("click", () => {
           window.location.reload();
         });
+      
       }
     
   
